8d3cd97eb6d700055210ff39aaa59de4b39d66b9
Aishwarya Gunasekar <aish.gunashekar@gmail.com>
2017-05-13 22:47:50 +0530

    Updated Classifiernews.py

--- Classifiernews.py
+++ Classifiernews.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import re
 import string
-from tkinter import *
+import pickle
+from sklearn.externals import joblib
+
 # import matplotlib.pyplot as plt
 
 # the Naive Bayes model
@@ -16,32 +18,34 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 
 def comp():
-    column_names= ['ID','TITLE','URL','PUBLISHER','CATEGORY','STORY','HOSTNAME','TIMESTAMP']
-    news_df = pd.read_csv('newsCorpora.csv',sep='	',names=column_names)
-    X_independent = pd.DataFrame(news_df.iloc[1:5,:2].values)
-    X_independent = news_df.iloc[:,:2].values
-    headline = list(news_df["TITLE"])
-    headline1 = []
-    for elem in headline:
-        s = elem.lower()
-        trans = str.maketrans("","",string.punctuation)
-        s = s.translate(trans)
-        headline1.append(s)
-    news_df["FORM_HEADLINE"] = headline1
-    X_independent = news_df[["ID","FORM_HEADLINE"]]
-    Y_dependent = news_df["CATEGORY"]
-    encoder = LabelEncoder()
-    Y_dependent = encoder.fit_transform(Y_dependent)
-    vec = CountVectorizer()
-    x = vec.fit_transform(news_df["FORM_HEADLINE"])
-    x_train, x_test, y_train, y_test = train_test_split(x, Y_dependent, test_size=0.2)
-    nb = MultinomialNB()
-    nb.fit(x_train, y_train)
-    scr = nb.score(x_test,y_test)
-    print(scr*100)
-    x1 = x_test[:3,:]
-    h = list(news_df.iloc[1000:1003,8])
-    test1 = vec.transform(h)
-    y = nb.predict(test1)
-    t1.insert(END,y)
-    return(y,scr*100)
+        #column_names= ['ID','TITLE','URL','PUBLISHER','CATEGORY','STORY','HOSTNAME','TIMESTAMP']
+        news_df = pd.read_csv('Final_dataset.csv')
+        X_independent = pd.DataFrame(news_df.iloc[1:5,:2].values)
+        X_independent = news_df.iloc[:,:2].values
+        headline = list(news_df["TITLE"])
+        headline1 = []
+        for elem in headline:
+            s = elem.lower()
+            trans = str.maketrans("","",string.punctuation)
+            s = s.translate(trans)
+            headline1.append(s)
+        news_df["FORM_HEADLINE"] = headline1
+        X_independent = news_df[["ID","FORM_HEADLINE"]]
+        Y_dependent = news_df["CATEGORY"]
+        encoder = LabelEncoder()
+        Y_dependent = encoder.fit_transform(Y_dependent)
+        vec = CountVectorizer()
+        x = vec.fit_transform(news_df["FORM_HEADLINE"])
+        x_train, x_test, y_train, y_test = train_test_split(x, Y_dependent, test_size=0.2)
+        nb = MultinomialNB()
+        nb = nb.fit(x_train, y_train)
+        scr = nb.score(x_test,y_test)
+        print(scr)
+        filename = '/tmp/nb_classifier.joblib.pkl'
+        _ = joblib.dump(nb, filename, compress=9)
+        filename = '/tmp/vec_classifier.joblib.pkl'
+        _ = joblib.dump(vec, filename, compress=9)
+
+        return(nb,vec,scr*100)
+
+comp()
