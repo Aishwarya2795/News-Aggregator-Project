e3766a9a1ab5477b62047296ced31fcdd227cf68
Aishwarya Gunasekar <aish.gunashekar@gmail.com>
2017-05-15 16:53:37 +0530

    Tkinterquery with index

--- Tkinterquery.py
+++ Tkinterquery.py
@@ -1,6 +1,9 @@
 from elasticsearch import Elasticsearch
 from tkinter import *
 es = Elasticsearch()
+print("HELLO")
+cat_dct = {"e":"Sports and Entertainment","b":"Business and Finance","m":"Health","t":"Science and Technology"}
+
 
 window = Tk()
     #print("Would you like to search by:\n(1)Keyword\n(2)Category\n(3)Exit\n")
@@ -8,63 +11,70 @@ window = Tk()
     #if choice == "1":
 
 
+
 def keywords():
-    #list_keys["ta1"].insert(END,"HELLLO THIS THIS WORKING" )
-        #print("Enter keywords to search\n")
-        #body = input()
-        body = body1.get()
-        dct = {"query":{"match":{"headline":body}}}
-        res = es.search(index="article_index",doc_type="newspaper_articles",body=dct)
-        res_hits = res["hits"]["hits"]
-        count = 1
-        if len(res_hits) == 0:
-            #print("No results")
-            ta1.insert(END,"NO RESULTS")
-            ta2.insert(END,"NO RESULTS")
-            ta3.insert(END,"NO RESULTS")
-        else:
-            for elem in res_hits:
-            #print(elem["_source"]["headline"],"\n",elem["_source"]["url"],"\n",elem["_source"]["category"])
-            #ta1.insert(END,elem["_source"]["headline"])
-            #ta2.insert(END,elem["_source"]["url"])
-            #ta3.insert(END,elem["_source"]["category"])
-                list_keys["ta{}".format(count)].insert(END,elem["_source"]["headline"]) )
-                count = count + 1
-                list_keys["ta{}".format(count)].insert(END,elem["_source"]["url"]) )
-                count = count + 1
-                list_keys["ta{}".format(count)].insert(END,elem["_source"]["url"]) )
-                count = count + 1
+#list_keys["ta1"].insert(END,"HELLLO THIS THIS WORKING" )
+    #print("Enter keywords to search\n")
+    #body = input()
 
+    for x in range(1,16):
+        list_keys["ta{}".format(x)].delete(1.0,END)
 
+    body = body1.get()
+    dct = {"query":{"match":{"headline":body}}}
+    res = es.search(index="article_index",doc_type="newspaper_articles",body=dct)
+    res_hits = res["hits"]["hits"]
+    count = 1
+    if len(res_hits) == 0:
+    #print("No results")
+        list_keys["ta1"].insert(END,"NO RESULTS" )
+        count = count + 1
+        list_keys["ta2"].insert(END,"NO RESULTS" )
+        count = count + 1
+        list_keys["ta3"].insert(END,"NO RESULTS" )
 
+    for elem in res_hits:
+    #print(elem["_source"]["headline"],"\n",elem["_source"]["url"],"\n",elem["_source"]["category"])
+    #ta1.insert(END,elem["_source"]["headline"])
+    #ta2.insert(END,elem["_source"]["url"])
+    #ta3.insert(END,elem["_source"]["category"])
+        list_keys["ta{}".format(count)].insert(END,elem["_source"]["headline"])
+        count = count + 1
+        list_keys["ta{}".format(count)].insert(END,elem["_source"]["url"])
+        count = count + 1
+        list_keys["ta{}".format(count)].insert(END,cat_dct[elem["_source"]["category"]])
+        count = count + 1
 
-    #elif choice == "2":
+#elif choice == "2":
 def categories():
-    print("HI")
-        #print("Enter category to search.\nYour options are:\nb\ne\nm\nt\n")
-        #body = input()
-        body = body2.get()
-        dct = {"query":{"match":{"category":body}}}
-        res = es.search(index="article_index", doc_type="newspaper_articles", body=dct)
-        res_hits = res["hits"]["hits"]
-        if len(res_hits) == 0:
-            #print("No results")
-            ta1.insert(END,"NO RESULTS")
-            ta2.insert(END,"NO RESULTS")
-            ta3.insert(END,"NO RESULTS")
-        count = 1
-        for elem in res_hits:
-            #print(elem["_source"]["headline"], "\n", elem["_source"]["url"], "\n", elem["_source"]["category"])
-            list_keys["ta{}".format(count)].insert(END,elem["_source"]["headline"]) )
-            count = count + 1
-            list_keys["ta{}".format(count)]..insert(END,elem["_source"]["url"]) )
-            count = count + 1
-            list_keys["ta{}".format(count)].insert(END,elem["_source"]["url"]) )
-            count = count + 1
-#    else:
-#break
 
-
+#print("Enter category to search.\nYour options are:\nb\ne\nm\nt\n")
+#body = input()
+    for x in range(1,16):
+        list_keys["ta{}".format(x)].delete(1.0,END)
+    body = body2.get()
+    dct = {"query":{"match":{"category":body}}}
+    res = es.search(index="article_index", doc_type="newspaper_articles", body=dct)
+    res_hits = res["hits"]["hits"]
+    count = 1
+    if len(res_hits) == 0:
+        #print("No results")
+        list_keys["ta1"].insert(END,"NO RESULTS" )
+        count = count + 1
+        list_keys["ta2"].insert(END,"NO RESULTS" )
+        count = count + 1
+        list_keys["ta3"].insert(END,"NO RESULTS" )
+    count = 1
+    for elem in res_hits:
+        #print(elem["_source"]["headline"], "\n", elem["_source"]["url"], "\n", elem["_source"]["category"])
+        list_keys["ta{}".format(count)].insert(END,elem["_source"]["headline"])
+        count = count + 1
+        list_keys["ta{}".format(count)].insert(END,elem["_source"]["url"])
+        count = count + 1
+        list_keys["ta{}".format(count)].insert(END,cat_dct[elem["_source"]["category"]])
+        count = count + 1
+    #    else:
+    #break
 
 def clear_entry_fields():
    e1.delete(0,END)
@@ -74,7 +84,7 @@ def clear_entry_fields():
 
 
 
-l1=Label(window,text="Keywords",height=5,width=8)
+l1=Label(window,text="Keywords",height=2,width=8,font=("Calibri", 25))
 l1.grid(row=0,column=0)
 body1=StringVar()
 e1=Entry(window,textvariable=body1)
@@ -82,7 +92,7 @@ e1.grid(row=0,column=1)  #Entry of the Keywords
 b1=Button(window,text="SEARCH",command=keywords)
 b1.grid(row=0,column=2)
 
-l2=Label(window,text="Category",height=5,width=8)
+l2=Label(window,text="Category",height=3,width=8,font=("Calibri", 25))
 l2.grid(row=1,column=0)
 body2=StringVar()
 e2=Entry(window,textvariable=body2)
@@ -93,19 +103,27 @@ b2.grid(row=1,column=2)
 b4=Button(window,text="CLEAR ALL",command=clear_entry_fields)
 b4.grid(row=11,column=1)
 
-Headline_Label=Label(window,text="Headline",height=5,width=8)
-Headline_Label.grid(row=2,column=0)
-Url_Label=Label(window,text="URL",height=5,width=8)
-Url_Label.grid(row=2,column=1)
-Category_Label=Label(window,text="Category",height=5,width=8)
-Category_Label.grid(row=2,column=2)
+Headline_Label=Label(window,text="Headline",height=5,width=8,font=("Calibri", 20))
+Headline_Label.grid(row=4,column=0)
+Url_Label=Label(window,text="URL",height=5,width=8,font=("Calibri", 20))
+Url_Label.grid(row=4,column=1)
+Category_Label=Label(window,text="Category",height=5,width=8,font=("Calibri", 20))
+Category_Label.grid(row=4,column=2)
 
+Business_Label=Label(window,text="b -> Business and Finance",height=1,width=30,font=("Calibri", 15))
+Business_Label.grid(row=2,column=0)
+Entertainment_Label=Label(window,text="e -> Entertainment and Sports",height=1,width=30,font=("Calibri", 15))
+Entertainment_Label.grid(row=2,column=1)
+Medical_Label=Label(window,text="m -> Medical",height=3,width=30,font=("Calibri", 15))
+Medical_Label.grid(row=3,column=0)
+Tech_Label=Label(window,text="t -> Technology",height=3,width=30,font=("Calibri", 15))
+Tech_Label.grid(row=3,column=1)
 
-row_no = 4
+row_no = 5
 list_keys = {}
 for x in range(1,16,3):
         for y in range(0,3):
-            list_keys.update({"ta{}".format(x) : Text(window,height=5,width=30)})
+            list_keys.update({"ta{}".format(x) : Text(window,height=5,width=30, font=("Calibri", 15))})
             list_keys["ta{}".format(x)].grid(row = row_no, column = y)
             x=x+1
         row_no = row_no + 1
@@ -148,3 +166,4 @@ ta15.grid(row=6,column=2) #
 
 
 window = mainloop()
+
